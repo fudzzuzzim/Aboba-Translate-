15f7149fad54b36b757fd53e78c7e9e578fc7d7d
fudzzuzzim <maksimborsky@gmail.com>
2022-03-21 21:15:21 +0300

    Прикрутил форму

--- js/index.js
+++ js/index.js
@@ -7,3 +7,9 @@ let next = () => {
 	setTimeout(() => body.style.cssText= "background-color: #ffffff;",10000);
 }
 button.addEventListener("click",next);
+let form = document.forms[0];
+let input = form.input;
+input.addEventListener("input",(event) => {
+	console.log(event.target.value);
+})
+
